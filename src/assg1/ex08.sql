ee9551cc7f78682277eecbdd4f0fdcb0c15ee261
Karol Belina <karolbelina@gmail.com>
2019-10-10 22:58:02 +0200

    Add the excercise #8

---
+++ src/assg1/ex08.sql
@@ -0,0 +1,7 @@
+SELECT imie AS "IMIE",
+       CASE
+         WHEN (NVL(przydzial_myszy, 0) + NVL(myszy_extra, 0)) * 12 < 660 THEN 'Ponizej 660'
+         WHEN (NVL(przydzial_myszy, 0) + NVL(myszy_extra, 0)) * 12 = 660 THEN 'Limit'
+         ELSE TO_CHAR((NVL(przydzial_myszy, 0) + NVL(myszy_extra, 0)) * 12)
+       END "Zjada rocznie"
+  FROM kocury;
