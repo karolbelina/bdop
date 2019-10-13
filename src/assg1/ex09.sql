562f9b51a4005f350f7c2010eedea69c4e9b984d
Karol Belina <karolbelina@gmail.com>
2019-10-13 22:35:04 +0200

    Change the formatting a bit

--- src/assg1/ex09.sql
+++ src/assg1/ex09.sql
@@ -7,13 +7,12 @@ SELECT pseudo AS "PSEUDO", w_stadku_od AS "W STADKU",
           in days between the target day and the last day of the month */
        -- the last day of the month can be obtained through the LAST_DAY function */
        -- to get the weekday of some day x, type TO_CHAR(x, 'DAY', 'NLS_DATE_LANGUAGE=''numeric date language''') */
-       AND MOD(TO_CHAR(LAST_DAY(SYSDATE), 'DAY', 'NLS_DATE_LANGUAGE=''numeric date language''') + 4, 7) < LAST_DAY(SYSDATE) - SYSDATE THEN
+       AND MOD(TO_CHAR(LAST_DAY(SYSDATE), 'DAY', 'NLS_DATE_LANGUAGE=''numeric date language''') + 4, 7) < LAST_DAY(SYSDATE) - SYSDATE
        /* the function for the last wednesday of the current month is 
           x - (x + 4) mod 7, where x is the last day of the month */
-       LAST_DAY(SYSDATE) - MOD(TO_CHAR(LAST_DAY(SYSDATE), 'DAY', 'NLS_DATE_LANGUAGE=''numeric date language''') + 4, 7)
-       ELSE
+       THEN LAST_DAY(SYSDATE) - MOD(TO_CHAR(LAST_DAY(SYSDATE), 'DAY', 'NLS_DATE_LANGUAGE=''numeric date language''') + 4, 7)
        /* to get the last wednesday of the next month just pass the
           LAST_DAY of the LAST_DAY + 1 to the function mentioned above */
-       LAST_DAY(LAST_DAY(SYSDATE) + 1) - MOD(TO_CHAR(LAST_DAY(LAST_DAY(SYSDATE) + 1), 'DAY', 'NLS_DATE_LANGUAGE=''numeric date language''') + 4, 7)
+       ELSE LAST_DAY(LAST_DAY(SYSDATE) + 1) - MOD(TO_CHAR(LAST_DAY(LAST_DAY(SYSDATE) + 1), 'DAY', 'NLS_DATE_LANGUAGE=''numeric date language''') + 4, 7)
        END AS "WYPLATA"
   FROM kocury;
