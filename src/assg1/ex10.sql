91dff1c9c197994ae33898cc5eee3125e5313f0e
Karol Belina <karolbelina@gmail.com>
2019-10-13 17:20:54 +0200

    Add the exercises #10 and #11

---
+++ src/assg1/ex10.sql
@@ -0,0 +1,14 @@
+SELECT CASE
+         WHEN COUNT(*) = 1 THEN pseudo || ' - unikalny'
+         ELSE pseudo || ' - nieunikalny'
+       END AS "Unikalnosc atr. PSEUDO"
+  FROM kocury
+ GROUP BY pseudo;
+
+SELECT CASE
+         WHEN COUNT(*) = 1 THEN szef || ' - unikalny'
+         ELSE szef || ' - nieunikalny'
+       END AS "Unikalnosc atr. PSEUDO"
+  FROM kocury
+ GROUP BY szef
+HAVING szef IS NOT NULL; -- szef is nullable
---
+++ src/assg1/ex11.sql
@@ -0,0 +1,4 @@
+SELECT pseudo AS "Pseudonim", COUNT(*) AS "Liczba wrogow"
+  FROM wrogowie_kocurow
+ GROUP BY pseudo
+HAVING COUNT(*) >= 2;
