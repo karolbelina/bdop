1d5485f23840f045ad02450610e35710d77b23c3
Karol Belina <karolbelina@gmail.com>
2019-10-13 18:23:08 +0200

    Change the column name

--- src/assg1/ex10.sql
+++ src/assg1/ex10.sql
@@ -8,7 +8,7 @@ SELECT CASE
 SELECT CASE
          WHEN COUNT(*) = 1 THEN szef || ' - unikalny'
          ELSE szef || ' - nieunikalny'
-       END AS "Unikalnosc atr. PSEUDO"
+       END AS "Unikalnosc atr. SZEF"
   FROM kocury
  GROUP BY szef
 HAVING szef IS NOT NULL; -- szef is nullable
