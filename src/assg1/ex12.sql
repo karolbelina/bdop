0dd9a215d80eaa63e7a52ee8e51058889ae4e754
Karol Belina <karolbelina@gmail.com>
2019-10-14 14:03:50 +0200

    Get rid of the inequality in plec checking

--- src/assg1/ex12.sql
+++ src/assg1/ex12.sql
@@ -2,6 +2,6 @@ SELECT 'Liczba kotow=' AS " ", COUNT(*) AS " ", 'lowi jako' AS " ", funkcja AS "
        MAX(NVL(przydzial_myszy, 0) + NVL(myszy_extra, 0)) AS " ", 'myszy miesiecznie' AS " "
   FROM kocury
  WHERE funkcja != 'SZEFUNIO'
-   AND plec != 'M'
+   AND NVL(plec, 'D') = 'D' -- could be plec != 'M', but inequalities should be avoided
  GROUP BY funkcja
 HAVING AVG(NVL(przydzial_myszy, 0) + NVL(myszy_extra, 0)) > 50;
