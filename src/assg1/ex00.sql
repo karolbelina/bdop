60cbb087b0f4e182b477db03c83a36e31dc116c5
Karol Belina <karolbelina@gmail.com>
2019-10-14 14:33:53 +0200

    Add the correct table altering

--- src/assg1/ex00.sql
+++ src/assg1/ex00.sql
@@ -73,8 +73,38 @@ CREATE TABLE wrogowie_kocurow (
     CONSTRAINT wrk_pk PRIMARY KEY (pseudo, imie_wroga)
 );
 
-  ALTER TABLE bandy
-DISABLE CONSTRAINT ban_koc_pseudo_fk;
+  ALTER TABLE kocury
+DISABLE CONSTRAINT koc_fun_funkcja_fk;
+
+  ALTER TABLE kocury
+DISABLE CONSTRAINT koc_koc_pseudo_fk;
+
+  ALTER TABLE kocury
+DISABLE CONSTRAINT koc_ban_nr_bandy_fk;
+
+INSERT ALL
+  INTO kocury VALUES ('JACEK',   'M', 'PLACEK',   'LOWCZY',   'LYSY',   '2008-12-01', 67, NULL, 2)
+  INTO kocury VALUES ('BARI',    'M', 'RURA',     'LAPACZ',   'LYSY',   '2009-09-01', 56, NULL, 2)
+  INTO kocury VALUES ('MICKA',   'D', 'LOLA',     'MILUSIA',  'TYGRYS', '2009-10-14', 25, 47,   1)
+  INTO kocury VALUES ('LUCEK',   'M', 'ZERO',     'KOT',      'KURKA',  '2010-03-01', 43, NULL, 3)
+  INTO kocury VALUES ('SONIA',   'D', 'PUSZYSTA', 'MILUSIA',  'ZOMBI',  '2010-11-18', 20, 35,   3)
+  INTO kocury VALUES ('LATKA',   'D', 'UCHO',     'KOT',      'RAFA',   '2011-01-01', 40, NULL, 4)
+  INTO kocury VALUES ('DUDEK',   'M', 'MALY',     'KOT',      'RAFA',   '2011-05-15', 40, NULL, 4)
+  INTO kocury VALUES ('MRUCZEK', 'M', 'TYGRYS',   'SZEFUNIO', NULL,     '2002-01-01', 103, 33,  1)
+  INTO kocury VALUES ('CHYTRY',  'M', 'BOLEK',    'DZIELCZY', 'TYGRYS', '2002-05-05', 50, NULL, 1)
+  INTO kocury VALUES ('KOREK',   'M', 'ZOMBI',    'BANDZIOR', 'TYGRYS', '2004-03-16', 75, 13,   3)
+  INTO kocury VALUES ('BOLEK',   'M', 'LYSY',     'BANDZIOR', 'TYGRYS', '2006-08-15', 72, 21,   2)
+  INTO kocury VALUES ('ZUZIA',   'D', 'SZYBKA',   'LOWCZY',   'LYSY',   '2006-07-21', 65, NULL, 2)
+  INTO kocury VALUES ('RUDA',    'D', 'MALA',     'MILUSIA',  'TYGRYS', '2006-09-17', 22, 42,   1)
+  INTO kocury VALUES ('PUCEK',   'M', 'RAFA',     'LOWCZY',   'TYGRYS', '2006-10-15', 65, NULL, 4)
+  INTO kocury VALUES ('PUNIA',   'D', 'KURKA',    'LOWCZY',   'ZOMBI',  '2008-01-01', 61, NULL, 3)
+  INTO kocury VALUES ('BELA',    'D', 'LASKA',    'MILUSIA',  'LYSY',   '2008-02-01', 24, 28,   2)
+  INTO kocury VALUES ('KSAWERY', 'M', 'MAN',      'LAPACZ',   'RAFA',   '2008-07-12', 51, NULL, 4)
+  INTO kocury VALUES ('MELA',    'D', 'DAMA',     'LAPACZ',   'RAFA',   '2008-11-01', 51, NULL, 4)
+SELECT * FROM DUAL;
+
+ ALTER TABLE kocury
+ENABLE CONSTRAINT koc_koc_pseudo_fk;
 
 INSERT ALL
   INTO bandy VALUES (1, 'SZEFOSTWO',       'CALOSC',  'TYGRYS')
@@ -83,6 +113,9 @@ INSERT ALL
   INTO bandy VALUES (4, 'LACIACI MYSLIWI', 'GORKA',   'RAFA'  )
   INTO bandy VALUES (5, 'ROCKERSI',        'ZAGRODA', NULL    )
 SELECT * FROM DUAL;
+
+ ALTER TABLE kocury
+ENABLE CONSTRAINT koc_ban_nr_bandy_fk;
 
 INSERT ALL
   INTO funkcje VALUES ('SZEFUNIO', 90, 110)
@@ -95,6 +128,9 @@ INSERT ALL
   INTO funkcje VALUES ('HONOROWA', 6,  25 )
 SELECT * FROM DUAL;
 
+ ALTER TABLE kocury
+ENABLE CONSTRAINT koc_fun_funkcja_fk;
+
 INSERT ALL
   INTO wrogowie VALUES ('KAZIO',          10, 'CZLOWIEK', 'FLASZKA'      )
   INTO wrogowie VALUES ('GLUPIA ZOSKA',   1,  'CZLOWIEK', 'KORALIK'      )
@@ -106,27 +142,6 @@ INSERT ALL
   INTO wrogowie VALUES ('CHYTRUSEK',      5,  'LIS',      'KURCZAK'      )
   INTO wrogowie VALUES ('SMUKLA',         1,  'SOSNA',    NULL           )
   INTO wrogowie VALUES ('BAZYLI',         3,  'KOGUT',    'KURA DO STADA')
-SELECT * FROM DUAL;
-
-INSERT ALL
-  INTO kocury VALUES ('MRUCZEK', 'M', 'TYGRYS',   'SZEFUNIO', NULL,     '2002-01-01', 103, 33,  1)
-  INTO kocury VALUES ('BOLEK',   'M', 'LYSY',     'BANDZIOR', 'TYGRYS', '2006-08-15', 72, 21,   2)
-  INTO kocury VALUES ('JACEK',   'M', 'PLACEK',   'LOWCZY',   'LYSY',   '2008-12-01', 67, NULL, 2)
-  INTO kocury VALUES ('BARI',    'M', 'RURA',     'LAPACZ',   'LYSY',   '2009-09-01', 56, NULL, 2)
-  INTO kocury VALUES ('MICKA',   'D', 'LOLA',     'MILUSIA',  'TYGRYS', '2009-10-14', 25, 47,   1)
-  INTO kocury VALUES ('KOREK',   'M', 'ZOMBI',    'BANDZIOR', 'TYGRYS', '2004-03-16', 75, 13,   3)
-  INTO kocury VALUES ('PUNIA',   'D', 'KURKA',    'LOWCZY',   'ZOMBI',  '2008-01-01', 61, NULL, 3)
-  INTO kocury VALUES ('LUCEK',   'M', 'ZERO',     'KOT',      'KURKA',  '2010-03-01', 43, NULL, 3)
-  INTO kocury VALUES ('SONIA',   'D', 'PUSZYSTA', 'MILUSIA',  'ZOMBI',  '2010-11-18', 20, 35,   3)
-  INTO kocury VALUES ('PUCEK',   'M', 'RAFA',     'LOWCZY',   'TYGRYS', '2006-10-15', 65, NULL, 4)
-  INTO kocury VALUES ('LATKA',   'D', 'UCHO',     'KOT',      'RAFA',   '2011-01-01', 40, NULL, 4)
-  INTO kocury VALUES ('DUDEK',   'M', 'MALY',     'KOT',      'RAFA',   '2011-05-15', 40, NULL, 4)
-  INTO kocury VALUES ('CHYTRY',  'M', 'BOLEK',    'DZIELCZY', 'TYGRYS', '2002-05-05', 50, NULL, 1)
-  INTO kocury VALUES ('ZUZIA',   'D', 'SZYBKA',   'LOWCZY',   'LYSY',   '2006-07-21', 65, NULL, 2)
-  INTO kocury VALUES ('RUDA',    'D', 'MALA',     'MILUSIA',  'TYGRYS', '2006-09-17', 22, 42,   1)
-  INTO kocury VALUES ('BELA',    'D', 'LASKA',    'MILUSIA',  'LYSY',   '2008-02-01', 24, 28,   2)
-  INTO kocury VALUES ('KSAWERY', 'M', 'MAN',      'LAPACZ',   'RAFA',   '2008-07-12', 51, NULL, 4)
-  INTO kocury VALUES ('MELA',    'D', 'DAMA',     'LAPACZ',   'RAFA',   '2008-11-01', 51, NULL, 4)
 SELECT * FROM DUAL;
 
 INSERT ALL
@@ -149,8 +164,5 @@ INSERT ALL
   INTO wrogowie_kocurow VALUES ('MALY',     'CHYTRUSEK',      '2011-07-13', 'PODEBRAL PODEBRANE JAJKA'                 )
   INTO wrogowie_kocurow VALUES ('UCHO',     'SWAWOLNY DYZIO', '2011-07-14', 'OBRZUCIL KAMIENIAMI'                      )
 SELECT * FROM DUAL;
-
- ALTER TABLE bandy
-ENABLE CONSTRAINT ban_koc_pseudo_fk;
 
 COMMIT;
