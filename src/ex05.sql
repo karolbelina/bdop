198356f27f17ce049366d4eb6ab994de4e906ae5
Karol Belina <karolbelina@gmail.com>
2019-10-21 19:42:30 +0200

    Slightly improve the solution to the exercise #5

--- src/ex05.sql
+++ src/ex05.sql
@@ -1,10 +1,10 @@
 SELECT pseudo,
        SUBSTR(temp, 1, INSTR(temp, 'L') - 1)
-       || REPLACE(SUBSTR(temp, INSTR(temp, 'L'), 1), 'L', '%')
-       || SUBSTR(temp, INSTR(temp, 'L') + 1)
+       || '%'
+       || SUBSTR(temp, INSTR(temp, 'L') + 1) AS "Po wymianie A na # oraz L na %"
   FROM (SELECT pseudo,
                SUBSTR(pseudo, 1, INSTR(pseudo, 'A') - 1)
-               || REPLACE(SUBSTR(pseudo, INSTR(pseudo, 'A'), 1), 'A', '#')
+               || '#'
                || SUBSTR(pseudo, INSTR(pseudo, 'A') + 1) AS temp
           FROM kocury
          WHERE pseudo LIKE '%A%')
