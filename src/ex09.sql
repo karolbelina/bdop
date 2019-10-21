2e91f14262708f4081d2da380b02d3cce7b3f183
Karol Belina <karolbelina@gmail.com>
2019-10-21 20:25:23 +0200

    Modify the solution to use the NEXT_DAY function as it should do

--- src/ex09.sql
+++ src/ex09.sql
@@ -1,18 +1,13 @@
-SELECT pseudo AS "PSEUDO", w_stadku_od AS "W STADKU",
-       CASE
-       WHEN EXTRACT(DAY FROM w_stadku_od) <= 15
-       -- check if we're getting another wednesday this month
-       /* we do if (x + 4) mod 7 < d, where x is a weekday of the last day
-          of the month (values between 1 and 7), and d is the absolute difference
-          in days between the target day and the last day of the month */
-       -- the last day of the month can be obtained through the LAST_DAY function */
-       -- to get the weekday of some day x, type TO_CHAR(x, 'DAY', 'NLS_DATE_LANGUAGE=''numeric date language''') */
-       AND MOD(TO_CHAR(LAST_DAY(SYSDATE), 'DAY', 'NLS_DATE_LANGUAGE=''numeric date language''') + 4, 7) < LAST_DAY(SYSDATE) - SYSDATE
-       /* the function for the last wednesday of the current month is 
-          x - (x + 4) mod 7, where x is the last day of the month */
-       THEN LAST_DAY(SYSDATE) - MOD(TO_CHAR(LAST_DAY(SYSDATE), 'DAY', 'NLS_DATE_LANGUAGE=''numeric date language''') + 4, 7)
-       /* to get the last wednesday of the next month just pass the
-          LAST_DAY of the LAST_DAY + 1 to the function mentioned above */
-       ELSE LAST_DAY(LAST_DAY(SYSDATE) + 1) - MOD(TO_CHAR(LAST_DAY(LAST_DAY(SYSDATE) + 1), 'DAY', 'NLS_DATE_LANGUAGE=''numeric date language''') + 4, 7)
-       END AS "WYPLATA"
+SELECT pseudo AS "PSEUDO",
+       w_stadku_od AS "W STADKU",
+       -- NEXT_DAY(LAST_DAY(x) - 7, 3) gets you the last wednesday of the month from the date x
+       NEXT_DAY(LAST_DAY(
+           CASE
+           WHEN EXTRACT(DAY FROM w_stadku_od) <= 15
+           -- check if we're getting another wednesday this month
+           AND NEXT_DAY(LAST_DAY(SYSDATE) - 7, 3) >= SYSDATE
+           THEN SYSDATE
+           ELSE ADD_MONTHS(SYSDATE, 1)
+           END
+       ) - 7, 3) AS "WYPLATA"
   FROM kocury;
