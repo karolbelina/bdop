d9192ec84954ad6c5c8a7606189d429fe54b07fe
Karol Belina <karolbelina@gmail.com>
2020-01-12 02:03:29 +0100

    Fix the indentation

--- src/ex00.sql
+++ src/ex00.sql
@@ -11,13 +11,13 @@ DROP TABLE funkcje;
 
 CREATE TABLE bandy (
   nr_bandy   NUMBER(2)
-              CONSTRAINT ban_pk PRIMARY KEY,
+             CONSTRAINT ban_pk PRIMARY KEY,
   nazwa      VARCHAR2(20)
-              CONSTRAINT ban_nazwa_nn NOT NULL,
+             CONSTRAINT ban_nazwa_nn NOT NULL,
   teren      VARCHAR2(15)
-              CONSTRAINT ban_nazwa_uq UNIQUE,
+             CONSTRAINT ban_nazwa_uq UNIQUE,
   szef_bandy VARCHAR2(15)
-              CONSTRAINT ban_szef_bandy_uq UNIQUE
+             CONSTRAINT ban_szef_bandy_uq UNIQUE
 );
 
 CREATE TABLE funkcje (
@@ -32,9 +32,9 @@ CREATE TABLE funkcje (
 
 CREATE TABLE wrogowie (
   imie_wroga       VARCHAR2(15)
-                    CONSTRAINT wro_pk PRIMARY KEY,
+                   CONSTRAINT wro_pk PRIMARY KEY,
   stopien_wrogosci NUMBER(2)
-                    CONSTRAINT wro_stopien_wrogosci_ch CHECK(stopien_wrogosci BETWEEN 1 AND 10),
+                   CONSTRAINT wro_stopien_wrogosci_ch CHECK(stopien_wrogosci BETWEEN 1 AND 10),
   gatunek          VARCHAR2(15),
   lapowka          VARCHAR2(20)
 );
@@ -64,13 +64,13 @@ ALTER TABLE bandy
 
 CREATE TABLE wrogowie_kocurow (
   pseudo         VARCHAR2(15)
-                  CONSTRAINT wrk_koc_pseudo_fk REFERENCES kocury(pseudo),
+                 CONSTRAINT wrk_koc_pseudo_fk REFERENCES kocury(pseudo),
   imie_wroga     VARCHAR2(15)
-                  CONSTRAINT wrk_wro_imie_wroga_fk REFERENCES wrogowie(imie_wroga),
+                 CONSTRAINT wrk_wro_imie_wroga_fk REFERENCES wrogowie(imie_wroga),
   data_incydentu DATE
-                  CONSTRAINT wrk_data_incydentu_nn NOT NULL,
+                 CONSTRAINT wrk_data_incydentu_nn NOT NULL,
   opis_incydentu VARCHAR2(50),
-  CONSTRAINT wrk_pk PRIMARY KEY (pseudo, imie_wroga)
+                 CONSTRAINT wrk_pk PRIMARY KEY (pseudo, imie_wroga)
 );
 
   ALTER TABLE bandy
